a3d39bfd26526103859276b787a3faf1119baa64
Peter-Jan Celis <pj@celis.org>
2012-11-26 18:32:49 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Celis::Application.config.secret_token = '0ce2821f23cf32e2b3357eca0031393c6833ac64290adbdb324ba84ab2bee918392d8edc00fe12f7c41332cb92ab9c5b672d80f0c7e4be353b57566caec5a9b9'
